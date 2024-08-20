130b2f8540832d3360126642c90b7bdb2e247858
Daniil Zotov <danjke99@yandex.ru>
2024-08-20 12:40:06 +0300

    hello massage was changed

--- src/main/java/ru/danjke99/Main.java
+++ src/main/java/ru/danjke99/Main.java
@@ -2,6 +2,6 @@ package ru.danjke99;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi qa_guru!!!");
+        System.out.println("Bye bye qa_guru!!!");
     }
 }
